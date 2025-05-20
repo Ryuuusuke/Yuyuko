6d7fea67c923cf490efdd16f6a11b10983bcb9ce
Ryuuusuke <makewishmr@gmail.com>
2025-05-20 16:42:37 +0700

    Feat: Add send message after congrats

--- ranked/checkRank.js
+++ ranked/checkRank.js
@@ -3,26 +3,13 @@ const activeQuizzes = new Map(); // Menyimpan userId yang sedang ikut quiz
 const kotobaBotId = "251239170058616833";
 const level1RoleId = "1372825348189978665";
 
-/**
- * Menyimpan user yang mulai quiz
- */
 function trackUserQuizStart(message) {
     if (message.content === "k!quiz testlevel1 hardcore mmq=3") {
-        console.log(`[Quiz] ${message.author.tag} memulai quiz`);
-        activeQuizzes.set(message.author.id, Date.now()); // bisa pakai Date.now() untuk tracking waktu
+        activeQuizzes.set(message.author.id, Date.now());
     }
 }
 
-/**
- * Cek apakah pesan embed dari Kotoba Bot berisi skor & tambahkan role
- */
 async function checkRank(message) {
-    if (!message.embeds.length) {
-        console.log("bukan kotoba bot");
-    } else {
-        console.log("kotoba bot nih");
-    }
-
     for (const embed of message.embeds) {
         console.log(embed.description);
         if (
@@ -31,25 +18,20 @@ async function checkRank(message) {
         ) {
             // Ambil userId dari Map
             const [userId] = activeQuizzes.keys();
-            if (!userId) {
-                console.log(
-                    "[Quiz] Tidak ada user yang sedang mengikuti quiz.",
-                );
-                return;
-            }
+            if (!userId) return;
 
             try {
                 const member = await message.guild.members.fetch(userId);
                 await member.roles.add(level1RoleId);
-                console.log(
-                    `[Quiz] Role Level 1 diberikan ke ${member.user.tag}`,
-                );
+
+                message.channel.send({
+                    content: `<@${member.id}> Congratulations for you just finished Level 1 ranked quiz!`,
+                });
                 activeQuizzes.delete(userId);
             } catch (err) {
                 console.error("[Quiz] Gagal memberikan role:", err);
             }
-
-            break; // keluar dari loop jika sudah cocok
+            break;
         }
     }
 }
